8572bdca85f0c6fefb6d75341809219bb36a251b
deannaterry <dterry@usgs.gov>
2016-10-27 16:47:14 -0500

    Created new project for SIMS/RMS redevelopment

---
+++ SIMS2017/RMS.Master.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SIMS2017
+{
+    public partial class RMS : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SIMS2017/SIMS.Master.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SIMS2017
+{
+    public partial class SIMS : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
