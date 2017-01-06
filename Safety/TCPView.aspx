22a0a1bb9c0a3071b19c25d81d7f383f74f6dba8
deannaterry <dterry@usgs.gov>
2017-01-06 13:17:20 -0600

    Created the TCP View

---
+++ Safety/TCPView.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SafetySingleMenu.Master" AutoEventWireup="true" CodeBehind="TCPView.aspx.cs" Inherits="Safety.TCPView" %>
+<%@ Register Src="~/Control/SitePageHeading.ascx" TagName="PageHeading" TagPrefix="uc" %>
+<%@ Register assembly="Telerik.Web.UI" namespace="Telerik.Web.UI" tagprefix="telerik" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="styles/reports.css" rel="stylesheet" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cph1" runat="server">
+    <uc:PageHeading id="ph1" runat="server" />
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="cph2" runat="server">
+
+    <!-- ADD THE TRAFFIC CONTROL PLAN IN THIS CONTENT PLACE HOLDER -->
+
+</asp:Content>
---
+++ Safety/TCPView.aspx.cs
@@ -0,0 +1,88 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Safety
+{
+    public partial class TCPView : System.Web.UI.Page
+    {
+        #region Local Variables
+        private Data.SIMSDataContext db = new Data.SIMSDataContext();
+        public WindowsAuthenticationUser user = new WindowsAuthenticationUser();
+        private Data.TCP currTCP;
+        private int WSCID
+        {
+            get
+            {
+                if (Session["WSCID"] == null) return 0; else return (int)Session["WSCID"];
+            }
+            set
+            {
+                Session["WSCID"] = value;
+            }
+        }
+        private int OfficeID
+        {
+            get
+            {
+                if (Session["OfficeID"] == null) return 0; else return (int)Session["OfficeID"];
+            }
+            set
+            {
+                Session["OfficeID"] = value;
+            }
+        }
+        private int TCPID
+        {
+            get
+            {
+                if (Session["TCPID"] == null) return 0; else return (int)Session["TCPID"];
+            }
+            set
+            {
+                Session["TCPID"] = value;
+            }
+        }
+        private int SiteID
+        {
+            get
+            {
+                if (Session["SiteID"] == null) return 0; else return (int)Session["SiteID"];
+            }
+            set
+            {
+                Session["SiteID"] = value;
+            }
+        }
+        #endregion
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //If no TCPID was passed, then redirect back to the homepage
+            string tcp_id = Request.QueryString["TCPID"];
+            if (!string.IsNullOrEmpty(tcp_id)) TCPID = Convert.ToInt32(tcp_id); else Response.Redirect(Config.SIMS2017URL + "SIMSWSCHome.aspx");
+
+            //Using the passed TCPID, setup the TCP data element, and reset the office and wsc to match that of the current site
+            currTCP = db.TCPs.Where(p => p.TCPID == TCPID).FirstOrDefault();
+            SiteID = (int)currTCP.site_id;
+            OfficeID = (int)currTCP.TCPSite.Site.office_id;
+            WSCID = (int)db.Offices.Where(p => p.office_id == OfficeID).FirstOrDefault().wsc_id;
+
+            //--BASIC PAGE SETUP--------------------------------------------------------------------
+            ph1.Title = currTCP.TCPPlanDetail.Number + " - TCP, " + currTCP.TCPPlanDetail.SubName;
+            ph1.SubTitle = currTCP.TCPSite.Site.site_no + " " + currTCP.TCPSite.Site.station_full_nm;
+
+            //Fill the page with data
+            PopulateReport();
+        }
+
+        protected void PopulateReport()
+        {
+
+        }
+    }
+}
